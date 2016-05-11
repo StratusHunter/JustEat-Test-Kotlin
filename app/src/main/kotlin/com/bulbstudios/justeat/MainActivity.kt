00c44146873395c21143417e363c3fb4124bf0db
Terence Baker <terence@bulbstudios.com>
2016-05-11 14:45:20 +0100

    Basic empty kotlin project

---
+++ app/src/main/kotlin/com/bulbstudios/justeat/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.bulbstudios.justeat
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by Terence Baker on 11/05/2016.
+ */
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
