c09e9774f2ae55a28eea930e8f645b7f191e100f
Terence Baker <terence@bulbstudios.com>
2016-05-12 10:02:40 +0100

    Search callbacks now working. Attempting to bind data to recyclerview using rx

--- app/src/main/kotlin/com/bulbstudios/justeat/activities/MainActivity.kt
+++ app/src/main/kotlin/com/bulbstudios/justeat/activities/MainActivity.kt
@@ -3,17 +3,27 @@ package com.bulbstudios.justeat.activities
 import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.SearchView
 import android.util.Log
 import android.view.Menu
 import com.bulbstudios.justeat.R
+import com.bulbstudios.justeat.dataclasses.Resturant
+import com.jakewharton.rxbinding.support.v7.widget.RxRecyclerView
+import com.jakewharton.rxbinding.support.v7.widget.RxRecyclerViewAdapter
 import com.jakewharton.rxbinding.support.v7.widget.queryTextChanges
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.subscriptions.CompositeSubscription
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Terence Baker on 11/05/2016.
  */
 class MainActivity : AppCompatActivity() {
+
+    private val subscriptionList = CompositeSubscription()
+    private val resturantDataSource = RxDataSource<Resturant>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -28,15 +38,51 @@ class MainActivity : AppCompatActivity() {
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
 
         val searchItem = menu.findItem(R.id.search)
-        val searchView = searchItem.actionView as? SearchView
+        val searchView = MenuItemCompat.getActionView(searchItem) as? SearchView
 
         if (searchView != null) {
 
-            searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName));
-
-            searchView.queryTextChanges().subscribe { text -> Log.d("blah", "stuff: " + text) }
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            subscribeSearchChange(searchView)
         }
 
         return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+        subscriptionList.clear()
+    }
+
+    private fun clearSearch() {
+
+    }
+
+    private fun searchWithPostcode(text: String) {
+
+    }
+
+
+    private fun bindArrayToRecyclerView() {
+
+        RxRecyclerViewAdapter.dataChanges(resturantDataSource)
+        recyclerView.
+    }
+
+    private fun subscribeSearchChange(searchView: SearchView) {
+
+        subscriptionList.add(searchView.queryTextChanges()
+                                     .throttleWithTimeout(400, TimeUnit.MILLISECONDS)
+                                     .distinctUntilChanged()
+                                     .map { postcode ->
+
+                                         postcode.toString()
+                                     }
+                                     .subscribe { text ->
+
+                                         if (text.length == 0) clearSearch() else searchWithPostcode(text)
+                                     })
+    }
 }
+
---
+++ app/src/main/kotlin/com/bulbstudios/justeat/dataclasses/Resturant.kt
@@ -0,0 +1,7 @@
+package com.bulbstudios.justeat.dataclasses
+
+/**
+ * Created by Terence Baker on 12/05/2016.
+ */
+class Resturant {
+}
